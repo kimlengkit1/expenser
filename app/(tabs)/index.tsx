29096cf89009e50fec11ebd819503177f643aacc
Benson Zheng <bensonzheng2003@gmail.com>
2024-11-09 16:16:10 -0500

    testing some functions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,37 @@
-import { Image, View, Text} from 'react-native';
+import { Image, View, Text, StyleSheet, Button, Alert} from 'react-native';
 
 
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>
+    <View style={styles.center}>
+      <Text style={styles.red} >
         Hi!
       </Text>
+      <Button title="Press Me" onPress={()=>
+        Alert.alert(
+          "Chris",
+          "topher Chen~",
+          [
+            { text: "Chest", onPress: () => console.log("Chest pressed") },
+            { text: "Leg", onPress: () => console.log("Leg pressed") }
+          ]
+        )
+        
+      } color="red"/>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  red: {
+    color: "red",
+    fontSize: 32
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    
+  }
+});
