56037ba1f8bd19789a36cd2a3d7a564cb436550f
Benson Zheng <bensonzheng2003@gmail.com>
2024-11-10 00:47:17 -0500

    current

--- app/lol2.tsx
+++
@@ -1,9 +0,0 @@
-import { Image, View, Text, StyleSheet, Button, Alert} from 'react-native';
-export default function HomeScreen() {
-    return (
-      <View >
-      <Text> why</Text>
-      </View>
-    );
-  }
-  
